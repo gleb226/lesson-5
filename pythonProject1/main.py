8feb3d405bb3f465272be6aa3e752ec60e1cd250
gleb226 <gmedvede226@gmail.com>
2023-04-15 10:20:51 +0300

    got neme, surname

--- pythonProject1/main.py
+++ pythonProject1/main.py
@@ -10,3 +10,8 @@ class Person:
     def __init__(self, name, surname):
         self.name = name
         self.surname = surname
+
+api_name = person['results'][0]['name']['first']
+api_surname = person['results'][0]['name']['last']
+
+print(api_name, api_surname)
